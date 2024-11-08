378f5da44fbf924be90236b37dea94639f174750
gabiellima-wav <seuemail@example.com>
2024-11-07 22:21:25 -0300

    commit inicial

--- app/src/main/java/com/example/gs_mobile/MainActivity.kt
+++ app/src/main/java/com/example/gs_mobile/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.gs_mobile
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Configuração de margens para barras do sistema
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Carrega e inicia a animação de rotação na logo
+        val logoImageView = findViewById<ImageView>(R.id.logoImageView)
+        val rotateAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate_logo)
+        logoImageView.startAnimation(rotateAnimation)
+
+        // Configura o botão para navegar para LoginActivity
+        val btnEntrar = findViewById<Button>(R.id.btnEntrar)
+        btnEntrar.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
